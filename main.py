76fd68cd75c9aa57e7d751f1668247814e88c2d0
anil <anilsai029@gmail.com>
2022-07-25 18:13:22 +0530

    added main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("inside main.py")
+import mlflow
+
+def main():
+    with mlflow.start_run():
+        mlflow.run(".","stage1",use_conda=False)
+        mlflow.run(".","stage2",use_conda=False)
+        mlflow.run(".","stage3",use_conda=False)
+
+
+
+
+if __name__=="__main__":
+    main()
